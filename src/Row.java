6b3a19403d2915401fe9d3c6189e0e307a6f0a5b
Mamdouh_Nafee <mamdouhdido5@gmail.com>
2025-12-21 18:37:32 +0200

    added + modified old rows/colums classes cause they are called

---
+++ src/Row.java
@@ -0,0 +1,27 @@
+import java.util.Map;
+
+public class Row extends GridElement {
+    public Row(int rowNumber) {
+        super(rowNumber, Type.ROW);
+    }
+
+    public boolean scan() {
+        for (int i = 0; i < 9; i++) {
+            locations[grid[rowNumber][i]].append(Integer.toString(i + 1));
+            status &= locations[grid[rowNumber][i]].length() == 1;
+        }
+        return status;
+    }
+    public boolean scan(Map<Integer,Integer> emptyCells, int[] permutation) {
+        for (int i = 0; i < 9; i++) {
+            int x = grid[rowNumber][i];
+            if(x == 0){
+                x = permutation[ emptyCells.get(rowNumber * 10 + i)];
+            }
+            locations[x].append(Integer.toString(i + 1));
+            status &= locations[x].length() == 1;
+        }
+        return status;
+    }
+
+}
