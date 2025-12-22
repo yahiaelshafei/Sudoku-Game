3cf0c550bdc8e2a64719f5be0826917ed643fdd1
hanyaashraf50-gif <hanyaashraf50@gmail.com>
2025-12-22 03:30:21 +0200

    main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Controllable controller = new ControllerFacade(new GameDriver());
+
+        SwingUtilities.invokeLater(() -> {
+            new SudokuGUI(controller);
+        });
+    }
+}
